bb3dae2fb1b5a12e3d2378fa1288c4bdd5aa9fff
Fabiana Farias <47903743+fabianafarias@users.noreply.github.com>
2021-02-13 12:02:54 -0300

    changing tracks

--- data.js
+++ data.js
@@ -1,21 +1,21 @@
 window.audios = [
     {
       title:
-        "Como começei a programar / Por que criamos a Rocketseat / Nossa Stack",
-      artist: "Diego Fernandes",
-      cover: "como-comecei.jpg",
-      file: "como-comecei.mp3"
+        "Indigo Night",
+      artist: "Tamino",
+      cover: "Tamino-Amir.jpg",
+      file: "indigo-night.mp3"
     },
     {
-      title: "5 dicas para uma carreira sólida como programador",
-      artist: "Diego Fernandes",
-      cover: "5-dicas-para-uma-carreira-solida-como-programador.jpg",
-      file: "5-dicas-para-uma-carreira-solida-como-programador.mp3"
+      title: "Love Remains",
+      artist: "Tal Wilkenfeld",
+      cover: "Tal-Love-Remains.jpg",
+      file: "killing-me.mp3"
     },
     {
-      title: "Júnior Pleno ou Sênior, qual a diferença?",
-      artist: "Diego Fernandes",
-      cover: "junior-pleno-ou-senior-qual-a-diferenca.jpg",
-      file: "junior-pleno-ou-senior-qual-a-diferenca.mp3"
+      title: "Joy Joy",
+      artist: "Kinga Glyk",
+      cover: "Kinga-Feelings.jpg",
+      file: "joy-joy.mp3"
     }
   ];
